ec6c8ea0e4c49841f466289446a12a6185d3aa27
alejandrobarrantes <ale13bc@outlook.com>
2022-11-06 17:05:26 -0600

    ..

--- src/main/java/rest/Login.java
+++ src/main/java/rest/Login.java
@@ -8,21 +8,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping(path = "api")
-public class UsuarioRest {
+@RequestMapping(path = "api/login")
+public class Login {
 
-    HttpServletRequest request;
-
+    static HttpSession session;
     @CrossOrigin(origins = {"http://localhost:8080","http://localhost:63342"})
-    @PostMapping("/login")
-    public Usuario login(@RequestBody Usuario usuario) throws Exception {
-        System.out.println("entraLogin");
+    @PostMapping()
+    public Usuario login(@RequestBody Usuario usuario,HttpServletRequest request) throws Exception {
+
             Usuario logged=new Usuario(); 
             UsuarioDao usuarioDao=new UsuarioDao();
             try {
+
                 logged= usuarioDao.traerUsuario(usuario.getUsuario(), usuario.getContrasena());
                 logged.setContrasena("");
-                //request.getSession(true).setAttribute("user", logged);
+                session=request.getSession();
+                session.setAttribute("user", logged);
                 return logged;
             } catch (Exception ex) {
                 
@@ -32,13 +33,13 @@ public class UsuarioRest {
     
    @GetMapping("/user")
    public Usuario prueba(){
-       HttpSession session = request.getSession(true);
+       //HttpSession session = request.getSession(true);
         return (Usuario) session.getAttribute("user");
    }
     
     @DeleteMapping
-    public void logout() {  
-        HttpSession session = request.getSession(true);
+    public void logout() {
+        //HttpSession session = request.getSession(true);
         session.removeAttribute("user");
         session.invalidate();
     }
